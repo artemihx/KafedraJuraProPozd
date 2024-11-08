c4b2fdb326b1ec36bfb6c5341e739924f8cfb908
TrueWizardia <kirkozlov7913@mail.ru>
2024-11-08 13:56:22 +0700

    composer

--- web-base/core/bootstrap.php
+++ web-base/core/bootstrap.php
@@ -2,18 +2,19 @@
 
 const DIR_CONFIG = '/../config';
 
+require_once  __DIR__ . '/../vendor/autoload.php';
 
-spl_autoload_register(function ($className) {
-    $paths = include __DIR__ . DIR_CONFIG . '/path.php';
-    $className = str_replace('\\', '/', $className);
-
-    foreach ($paths['classes'] as $path) {
-        $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$path/$className.php";
-        if (file_exists($fileName)) {
-            require_once $fileName;
-        }
-    }
-});
+//spl_autoload_register(function ($className) {
+//    $paths = include __DIR__ . DIR_CONFIG . '/path.php';
+//    $className = str_replace('\\', '/', $className);
+//
+//    foreach ($paths['classes'] as $path) {
+//        $fileName = $_SERVER['DOCUMENT_ROOT'] . "/$path/$className.php";
+//        if (file_exists($fileName)) {
+//            require_once $fileName;
+//        }
+//    }
+//});
 
 function getConfigs(string $path = DIR_CONFIG): array
 {
